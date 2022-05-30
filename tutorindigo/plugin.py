763e965d520428012d23b28e4465b57eff889b30
Régis Behmo <regis@behmo.com>
2022-05-30 21:49:20 +0200

    fix: actually render scss files in tutor env
    *.scss files were not rendered at all in the tutor environment because they are
    stored in a "partials" subdirectory.
    
    Here, we make use of the new pattern include/ignore filter to force the
    rendering of the indigo theme directory.
    
    Close #24.

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -1,5 +1,3 @@
-from glob import glob
-import os
 import pkg_resources
 
 from tutor import hooks
@@ -43,6 +41,9 @@ hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
         ("indigo", "build/openedx/themes"),
     ],
 )
+
+# Force the rendering of scss files, even though they are included in a "partials" directory
+hooks.Filters.ENV_PATTERNS_INCLUDE.add_item(r"indigo/lms/static/sass/partials/lms/theme/")
 
 # Load all configuration entries
 hooks.Filters.CONFIG_DEFAULTS.add_items(
