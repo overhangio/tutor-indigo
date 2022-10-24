97c706fd11236af1cccd32403fce6331ca127f2f
Régis Behmo <regis@behmo.com>
2022-10-24 14:36:18 +0200

    feat: upgrade to olive

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -43,7 +43,9 @@ hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
 )
 
 # Force the rendering of scss files, even though they are included in a "partials" directory
-hooks.Filters.ENV_PATTERNS_INCLUDE.add_item(r"indigo/lms/static/sass/partials/lms/theme/")
+hooks.Filters.ENV_PATTERNS_INCLUDE.add_item(
+    r"indigo/lms/static/sass/partials/lms/theme/"
+)
 
 # Load all configuration entries
 hooks.Filters.CONFIG_DEFAULTS.add_items(
