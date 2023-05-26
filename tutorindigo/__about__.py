c82083a95e79e1a1b5dda9d12fd6aef1e9409c7c
Régis Behmo <regis@behmo.com>
2023-05-26 18:26:28 +0200

    feat: label nightly version
    This is to address https://github.com/overhangio/tutor-mfe/issues/122
    As a consequence of this change, images will be tagged with a "-nightly"
    suffix. Next, we'll probably have to build them periodically in CI.

--- tutorindigo/__about__.py
+++ tutorindigo/__about__.py
@@ -1,7 +1,7 @@
 __version__ = "15.0.0"
 
 # Handle version suffix for nightly, just like tutor core.
-__version_suffix__ = ""
+__version_suffix__ = "nightly"
 
 if __version_suffix__:
     __version__ += "-" + __version_suffix__
