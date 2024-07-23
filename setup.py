d9d08196eb0989771d8dca3f3aba84dab6f7a5b5
Régis Behmo <regis@behmo.com>
2024-07-23 10:37:35 +0200

    build: fix readme content type warning

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
     maintainer_email="hina.khadim@arbisoft.com",
     description="Indigo theme plugin for Tutor",
     long_description=load_readme(),
+    long_description_content_type="text/x-rst",
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.8",
