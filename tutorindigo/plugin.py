377230239b8bb08fc2c433d4414bde52c954f3b0
hinakhadim <hina.khadim@arbisoft.com>
2024-11-20 18:45:29 +0500

    fix:  upgrade header to  v5.8.0 of openedx for sumac and make video icon visible on course about page

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -118,7 +118,7 @@ hooks.Filters.ENV_PATCHES.add_items(
         (
             f"mfe-dockerfile-post-npm-install-{mfe}",
             """
-            
+           
 RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
 RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^3.2.2'
 RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^2.2.2'
