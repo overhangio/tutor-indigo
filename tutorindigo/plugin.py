773902518046b062ca008d18cac2d1aeb8dd3c34
Zubair Shakoor <zubair.shakoor@A006-00863.local>
2025-06-18 15:07:37 +0500

    fix: updated branches from custom header footer and brand deployment of indigo

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -119,9 +119,9 @@ hooks.Filters.ENV_PATCHES.add_items(
             f"mfe-dockerfile-post-npm-install-{mfe}",
             """
            
-RUN npm install @edly-io/indigo-frontend-component-footer@^2.0.0
-RUN npm install '@edx/frontend-component-header@npm:@edly-io/indigo-frontend-component-header@^3.2.2'
-RUN npm install '@edx/brand@npm:@edly-io/indigo-brand-openedx@^2.2.2'
+RUN npm install "https://github.com/edly-io/frontend-component-footer.git#teak/indigo"
+RUN npm install "https://github.com/edly-io/frontend-component-header.git#sumac/indigo"
+RUN npm install "https://github.com/edly-io/brand-openedx.git#sumac/indigo"
 
 """,
         )
