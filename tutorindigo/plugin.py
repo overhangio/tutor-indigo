62c1c1231c484ad89c62d476710b25168ad71841
arbirali <rahat.ali@arbisoft.com>
2026-07-07 18:02:56 +0500

    v21.2.0

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -123,7 +123,7 @@ for mfe in indigo_styled_mfes:
             (
                 f"mfe-dockerfile-post-npm-install-{mfe}",
                 """
-RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-2.5.3'
+RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-2.6.0'
 """,  # noqa: E501
             ),
         ]
@@ -132,7 +132,7 @@ RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-2.5.3'
 hooks.Filters.ENV_PATCHES.add_item(
     (
         "mfe-dockerfile-post-npm-install-authn",
-        "RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-2.5.3'",
+        "RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-2.6.0'",
     )
 )
 
