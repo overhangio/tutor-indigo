1fda5c6c98ecb443645f881d6d306e9f9178512e
arbirali <rahat.ali@arbisoft.com>
2026-09-23 13:41:38 +0500

    feat: Update tag to indigo-3.1.0 from indigo-3.0.0

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -124,7 +124,7 @@ for mfe in indigo_styled_mfes:
             (
                 f"mfe-dockerfile-post-npm-install-{mfe}",
                 """
-RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-3.0.0'
+RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-3.1.0'
 """,  # noqa: E501
             ),
         ]
@@ -133,7 +133,7 @@ RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-3.0.0'
 hooks.Filters.ENV_PATCHES.add_item(
     (
         "mfe-dockerfile-post-npm-install-authn",
-        "RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-3.0.0'",
+        "RUN npm install '@edx/brand@github:@edly-io/brand-openedx#indigo-3.1.0'",
     )
 )
 
